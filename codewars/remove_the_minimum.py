11da8c79633564eca5cde3cf8a0883a0c3682b9d
Anthony Rozon <arozonm@gmail.com>
2017-11-06 20:38:36 -0400

    Added remove_the_minimum solution

---
+++ codewars/remove_the_minimum.py
@@ -0,0 +1,22 @@
+# Task
+
+# Given an array of integers, remove the smallest value. 
+# Do not mutate the original array/list. If there are 
+# multiple elements with the same value, remove the one 
+# with a lower index. If you get an empty array/list, 
+# return an empty array/list. Don't change the order of 
+# the elements that are left.
+
+# My solution
+
+
+def remove_smallest(numbers):
+    if len(numbers) > 0:
+        smallest = numbers[0]
+        for x in range(1, len(numbers)):
+            if numbers[x] < smallest:
+                smallest = numbers[x]
+        numbers.remove(smallest)
+        return numbers
+    return []
+
